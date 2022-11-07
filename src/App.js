d35ac97d979c725977f7c6dc833fcfe3c4a11d71
Irene Riportella <riportellairene@gmail.com>
2022-11-07 09:07:32 +0100

    improuv animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './App.css';
 import Modal from './components/Modal/Modal';
@@ -8,6 +9,7 @@ import List from './components/List/List';
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
@@ -22,16 +24,50 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button
+          className="Button"
+          onClick={() => this.setState((prevState) => ({ showBlock: !prevState.showBlock }))}>
+          Toggle
+        </button>
+        <br />
+
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}>
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                transition: 'opacity 1s ease-out',
+                opacity: state === 'exited' ? 0 : state === 'entering' ? 0 : state === 'entered' ? 1 : 1,
+              }}></div>
+          )}
+        </Transition>
+
         <Modal
           show={this.state.modalIsOpen}
           closed={this.closeModal}
         />
-        <Backdrop show={this.state.modalIsOpen} />
+
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
+
         <button
           className="Button"
           onClick={this.showModal}>
           Open Modal
         </button>
+
         <h3>Animating Lists</h3>
         <List />
       </div>
